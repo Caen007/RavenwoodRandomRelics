46dd7fada4635532e39cffc6c92ccd325a584e60
Caen007 <czirjek.albert@gmail.com>
2026-05-22 10:32:14 +0300

    Initial Random Relics source

---
+++ HornOfThor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using Jotunn.Managers;
+
+namespace RavenwoodRandomRelics
+{
+    public class HornOfThor : MonoBehaviour, Hoverable, Interactable
+    {
+        private Piece m_piece;
+
+        private void Awake()
+        {
+            m_piece = GetComponent<Piece>();
+        }
+
+        public bool Interact(Humanoid user, bool hold, bool alt)
+        {
+            if (hold)
+            {
+                return false;
+            }
+
+            var sfx = ZNetScene.instance.GetPrefab("sfx_gjall_alerted");
+            if (sfx != null)
+            {
+                Instantiate(sfx, transform.position + Vector3.up * 1f, Quaternion.identity);
+            }
+
+            return true;
+        }
+
+        public bool UseItem(Humanoid user, ItemDrop.ItemData item)
+        {
+            return false;
+        }
+
+        public string GetHoverText()
+        {
+            string name = m_piece != null ? m_piece.m_name : "Horn of Thor";
+            return name + "\n[<color=yellow><b>E</b></color>] Blow Horn";
+        }
+
+        public string GetHoverName()
+        {
+            return m_piece != null ? m_piece.m_name : "Horn of Thor";
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RavenwoodRandomRelics")]
+[assembly: AssemblyDescription("A Valheim mod that adds custom relics, statues, and decor.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("James Jones TV")]
+[assembly: AssemblyProduct("Ravenwood Random Relics Mod")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e3243d22-4307-4008-ba36-9f326008cde5")]
+[assembly: AssemblyVersion("2.2.2")]
+[assembly: AssemblyFileVersion("2.2.2")]
